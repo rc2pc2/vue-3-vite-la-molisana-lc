c99c2dc48745f2679260e778bc6d1dab9b303239
rc2pc2 <petriccarcc@gmail.com>
2023-12-14 12:15:23 +0100

    added header first implementation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,52 @@
 <template lang="">
     <header>
-        <h1>
-            Header
-        </h1>
+        <section class="logo">
+            <img src="../assets/img/la-molisana-logo.png" alt="La Molisana main logo">
+        </section>
+        <nav class="navigation">
+            <ul>
+                <li v-for="link in navLinks" :class="(link.active) ? 'active' : ''">
+                    <a :href="link.url">
+                        {{ link.content }}
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
 <script>
 export default {
     name: 'AppHeader',
+    data() {
+        return {
+            navLinks : [
+                {
+                    content: 'Home',
+                    url: '#',
+                    active: false,
+                },
 
+                {
+                    content: 'Prodotti',
+                    url: '#',
+                    active: true,
+                },
+
+                {
+                    content: 'Chi Siamo',
+                    url: '#',
+                    active: false,
+                },
+
+                {
+                    content: 'Contatti',
+                    url: '#',
+                    active: false,
+                }
+            ],
+        }
+    },
 }
 </script>
 
@@ -20,5 +57,30 @@ export default {
 
 header{
     @include flex(row, center, center);
+    flex-wrap: wrap;
+    margin-bottom: 5rem;
+    section.logo{
+        width: 100%;
+        @include flex(row, center, center);
+        margin-bottom: 2.5rem;
+    }
+
+    nav.navigation > ul{
+        @include flex(row, center, center);
+        list-style-type: none;
+
+        li{
+            margin-right: 1.5rem;
+            font-size: 1.1rem;
+            padding: .9rem 1.15rem;
+            &.active{
+                background-color: $header-nav-bg;
+            }
+
+            &:hover{
+                background-color: $accent-color;
+            }
+        }
+    }
 }
 </style>
