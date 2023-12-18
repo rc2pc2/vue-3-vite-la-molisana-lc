65550b560b24e7cd67ad504be4a036b652539428
rc2pc2 <petriccarcc@gmail.com>
2023-12-18 09:56:55 +0100

    commented font-awesome usage due to unusage

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
-import '@fortawesome/fontawesome-free/scss/solid.scss';
+// import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+// import '@fortawesome/fontawesome-free/scss/solid.scss';
 
 createApp(App).mount('#app')
